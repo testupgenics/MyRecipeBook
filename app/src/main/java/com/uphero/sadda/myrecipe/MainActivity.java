2d1ef90f016b227430c85399f79e50eb7aa1f259
Sadda Hussain <SaddaHussain@AppsGenii.eu>
2018-01-24 00:42:28 +0500

    Updates

--- app/src/main/java/com/uphero/sadda/myrecipe/MainActivity.java
+++ app/src/main/java/com/uphero/sadda/myrecipe/MainActivity.java
@@ -25,9 +25,6 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
-import com.google.android.gms.ads.MobileAds;
 
 import org.json.JSONArray;
 import org.json.JSONException;
